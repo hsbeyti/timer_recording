eb3c0d6d1a0173dff5930a4afd17dffb144d71d5
hsbeyti <hsbeyti@gmail.com>
2022-05-11 14:29:01 +0200

    create New Rest API works well now

--- src/main/java/hsbeyti/time/recording/services/TimeRecordingServices.java
+++ src/main/java/hsbeyti/time/recording/services/TimeRecordingServices.java
@@ -12,8 +12,22 @@ public class TimeRecordingServices {
 	@Autowired
 	TimeRecordingRepository timeRecordingRepository ;
 
-	public CoWorkerWorkingTimeOnAProject createTimeRecording (CoWorkerWorkingTimeOnAProject coWorkerWorkingTimeOnAProject) {
-		return timeRecordingRepository.save(coWorkerWorkingTimeOnAProject);
+	public WorkingTime createTimeRecording (WorkingTime WorkingTimeOnAProject) {
+		// test if  a document allready exist for this project and this co-worker
+		// extract co-worker name
+		// extract project name
+		String workerName=WorkingTimeOnAProject.getCoWorkerName();
+		String projectName=WorkingTimeOnAProject.getProjectName();
+		WorkingTime found=isThereADocumentFor(workerName,projectName);
+		//System.out.println(found);
+		if(found==null)// not found then creat a new one
+		   return timeRecordingRepository.save(WorkingTimeOnAProject);
+		 
+	     return null;// document allreday exsist
+	}  
+	public WorkingTime isThereADocumentFor(String wroker_name, String project_name) {
+		WorkingTime found=timeRecordingRepository.findByCoWorkerNameAndProjectName(wroker_name,project_name);
+		return found;
 	}
 
 }
